c4c5b05ceb7579b86ce674492af1e3a2b3224db4
Khaled Mohamed <39915281+KhaledDev1@users.noreply.github.com>
2019-05-16 12:53:18 +0200

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,16 @@
+document.write("<p>hello jave script</p>");
+window.alert("Please note that this website is not compatable with mobile version.");
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+document.getElementById('demo').innerHTML = Date()
+
+document.getElementById('my-image').ondragstart = function() { return false; };
+
+//function dark() {
+  //      if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
+
+   //             document.body.style.backgroundColor = '#333';
+   //     }
+   //     else {
+     //           document.body.style.backgroundColor = 'rgb(255, 255, 255)';
+     //   }
+   // }
